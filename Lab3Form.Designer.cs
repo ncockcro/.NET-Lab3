3d1439d5420a8e54e57daf99a05dce20534c76cb
ncockcro <35208141+ncockcro@users.noreply.github.com>
2018-05-03 13:11:19 -0400

    Completed Lab 3
    Full implementation for taking strings entered by the user in the input text box and outputting them in the output text box using Microsoft Windows Forms.

---
+++ Lab3Form.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Lab3
+{
+    partial class Lab3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.computeButton = new System.Windows.Forms.Button();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(241, 12);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.inputTextBox.Size = new System.Drawing.Size(352, 316);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(241, 472);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputTextBox.Size = new System.Drawing.Size(352, 319);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.TextChanged += new System.EventHandler(this.outputTextBox_TextChanged);
+            // 
+            // computeButton
+            // 
+            this.computeButton.Location = new System.Drawing.Point(335, 358);
+            this.computeButton.Name = "computeButton";
+            this.computeButton.Size = new System.Drawing.Size(151, 91);
+            this.computeButton.TabIndex = 2;
+            this.computeButton.Text = "Compute";
+            this.computeButton.UseVisualStyleBackColor = true;
+            this.computeButton.Click += new System.EventHandler(this.computeButton_Click);
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(96, 142);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(65, 25);
+            this.inputLabel.TabIndex = 3;
+            this.inputLabel.Text = "Input:";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(96, 602);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(82, 25);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "Output:";
+            // 
+            // Lab3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 803);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.computeButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Lab3";
+            this.Text = "Lab3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button computeButton;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Lab3Form.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Written by: Nicholas Cockcroft
+ * Date: 2/1/18
+ * Course: .NET Environment
+ * Assignment: Lab 3
+ * 
+ * Description: Write a program to uses a text box for input and a button to indicate when the user has entered new data. 
+ * The data will be display on successive lines in a text box.
+ * Make sure that there is space for at least 10 lines and allow scrolling. 
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Lab3 : Form
+    {
+        public Lab3()
+        {
+            InitializeComponent();
+        }
+
+        private void computeButton_Click(object sender, EventArgs e)
+        {
+            // Store the input the user typed in to a string variable.
+            string sinput = inputTextBox.Text;
+
+            // Write user's input to the output text box.
+            outputTextBox.Text += sinput;
+            outputTextBox.Text += "\r\n";
+
+            // Clear the input text box.
+            inputTextBox.Text = "";
+
+        }
+
+        private void outputTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
